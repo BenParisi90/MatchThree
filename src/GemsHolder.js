e2fd64ba8a60a99d30d88bf877228104e7113c45
Benjamin Parisi <Ben@ParisiDesign.com>
2018-09-21 23:27:47 -0700

    deactive all gems not adjacent to selected gem

--- src/GemsHolder.js
+++ src/GemsHolder.js
@@ -35,12 +35,15 @@ exports = Class(ui.View, function (supr) {
 		/*this.style.width = 320;
 		this.style.height = 480;*/
 
-		this._gems = [];
+		//list of all gems as 1 dimensional array
+		this.gemsList = [];
+		//gird of gems as 2 dimensional array
+		this.gemsGrid = [];
 		this.inputState = "noSelection";
 
 		for (var row = 0; row < gemRows; row++) {
 			//add a new row of gems
-			this._gems.push([]);
+			this.gemsGrid.push([]);
 
 			for (var col = 0; col < gemCols; col++) {
 
@@ -53,7 +56,8 @@ exports = Class(ui.View, function (supr) {
 					gem.style.x = col * gem.style.width;
 					gem.style.y = row * (gem.style.height);
 					this.addSubview(gem);
-					this._gems[row].push(gem);
+					this.gemsGrid[row].push(gem);
+					this.gemsList.push(gem);
 					//update score on hit event
 					/*molehill.on('molehill:hit', bind(this, function () {
 						if (game_on) {
@@ -79,7 +83,7 @@ exports = Class(ui.View, function (supr) {
 		var yTar = yPos - 1;
 		var numMatches = 0;
 
-		while(xTar >= 0 && this._gems[yPos][xTar].gemType == gemType){
+		while(xTar >= 0 && this.gemsGrid[yPos][xTar].gemType == gemType){
 			numMatches ++;
 			xTar --;
 			if(numMatches >= 2){
@@ -88,7 +92,7 @@ exports = Class(ui.View, function (supr) {
 		}
 		
 		numMatches = 0;
-		while(yTar >= 0 && this._gems[yTar][xPos].gemType == gemType){
+		while(yTar >= 0 && this.gemsGrid[yTar][xPos].gemType == gemType){
 			numMatches ++;
 			yTar --;
 			if(numMatches >= 2){
@@ -100,8 +104,23 @@ exports = Class(ui.View, function (supr) {
 
 	this.selectGem = function(gem){
 		console.log("selected gem");
+		//this.inputState = "gemSelected";
+		//move the selected gem to the top of others
 		this.removeSubview(gem);
 		this.addSubview(gem);
-		this.inputState = "gemSelected";
+		//deactive all gems except one adjacent to selection
+		for(var i = 0; i < this.gemsList.length; i ++)
+		{
+			var targetGem = this.gemsList[i];
+			if((Math.abs(gem.xPos - targetGem.xPos) == 1 && gem.yPos == targetGem.yPos)
+				|| (gem.xPos == targetGem.xPos && Math.abs(gem.yPos - targetGem.yPos) == 1))
+			{
+				targetGem.activeInput = true;
+			}
+			else
+			{
+				targetGem.activeInput = false;
+			}
+		}
 	}
 });
